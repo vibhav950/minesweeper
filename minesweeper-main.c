385a6d214c7770e67844454e4df9e3823010ee8a
cygnus-UBT-VM <vibhavtiwari.official@gmail.com>
2023-11-16 19:33:18 +0530

    Made CLI

--- minesweeper-main.c
+++ minesweeper-main.c
@@ -1,121 +1,207 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include "rand.h"
+#include "colors.h"
 
-typedef struct cell {
-  int bomb;
-  int flag;
-  int clicked;
-  int value;
-} CELL;
+typedef enum _MSW_STATE_EN { FALSE, TRUE } MSW_BOOL;
+typedef enum _MSW_BOOL_EN { MSW_WIN, MSW_LOSS, MSW_NONE } MSW_STATE;
 
-typedef struct board {
+typedef struct _CELL_ST
+{
+  unsigned char bomb : 1;
+  unsigned char flag : 1;
+  unsigned char clicked : 1;
+  unsigned char hover : 1;
+  unsigned char value : 4;
+} MSW_CELL;
+
+typedef struct _BOARD_ST
+{
   int rows;
   int cols;
-  CELL **cells;
   int flags;
   int bombs;
-} BOARD;
+  MSW_CELL **cells;
+  int sx;
+  int sy;
+} MSW_BOARD;
 
-CELL *currboard;
-int bombstep = 0;
+typedef struct _CTX_ST
+{
+  MSW_BOOL bombstep;
+  int difficulty;
+  MSW_BOARD *board;
+  MSW_CELL *cur_board;
+} MSW_CTX;
 
-int checkwin(BOARD *board) {
-  CELL *cells = *board->cells;
-  if (bombstep)
-    return -1;
-  for (int i = 0; i < board->rows; i++) {
-    for (int j = 0; j < board->cols; j++) {
+MSW_STATE CheckWin (MSW_CTX *ctx)
+{
+  MSW_CELL *cells = *(ctx->board)->cells;
+  MSW_BOARD *board = ctx->board;
+
+  if (ctx->bombstep)
+  {
+    return MSW_LOSS;
+  }
+
+  for (int i = 0; i < board->rows; i++)
+  {
+    for (int j = 0; j < board->cols; j++)
+    {
       if (cells[i * board->cols + j].clicked == 0 &&
-          cells[i * board->cols + j].bomb == 0) {
-        return 0;
+          cells[i * board->cols + j].bomb == 0)
+      {
+        return MSW_NONE;
       }
     }
   }
-  return 1;
+
+  return MSW_WIN;
 }
 
-void select_difficulty(BOARD *board) {
-  printf("Select difficulty:\n");
-  printf("1. Easy (9*9 board with 10 bombs)\n");
-  printf("2. Medium (16*16 board with 40 bombs)\n");
-  printf("3. Hard (16*30 board with 99 bombs)\n");
-  printf("4. Custom\n");
-  int difficulty;
-  do {
-    scanf("%d", &difficulty);
-    if (difficulty < 1 || difficulty > 4)
-      printf("Invalid difficulty. Try again.\n");
-  } while (difficulty < 1 || difficulty > 4);
-  switch (difficulty) {
+void clr(void)
+{
+  printf(" \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ");
+}
+
+void printd(int diff)
+{
+  switch (diff)
+  {
   case 1:
-    board->rows = 9;
-    board->cols = 9;
+    printf("\r%s%s EASY %s  MEDIUM   HARD ", BG_CYAN_LIGHT, FG_BLACK, FG_BG_CLEAR);
     break;
   case 2:
-    board->rows = 16;
-    board->cols = 16;
+    printf("\r EASY  %s%s MEDIUM %s  HARD ", BG_CYAN_LIGHT, FG_BLACK, FG_BG_CLEAR);
     break;
   case 3:
-    board->rows = 16;
-    board->cols = 30;
-  case 4:
-    do {
-      printf("Enter number of rows: ");
-      scanf("%d", &board->rows);
-      printf("Enter number of columns: ");
-      scanf("%d", &board->cols);
-      if (board->rows < 1 || board->cols < 1)
-        printf("Invalid dimensions. Try again.\n");
-    } while (board->rows < 1 || board->cols < 1);
+    printf("\r EASY   MEDIUM  %s%s HARD %s", BG_RED_LIGHT, FG_BLACK, FG_BG_CLEAR);
     break;
   }
 }
 
-void num_bombs(BOARD *board) {
-  if (board->rows == 9 && board->cols == 9) {
+void SelectDifficulty(MSW_CTX *ctx)
+{
+  char c;
+  int difficulty = 1;
+
+  printd(difficulty);
+
+  while ((c = _getch()) != 13)
+  {
+    switch (c)
+    {
+    /* right arrow < */
+    case 97:
+      if (difficulty > 1)
+      {
+        difficulty--;
+      }
+      break;
+    /* left arrow > */
+    case 100:
+      if (difficulty < 3)
+      {
+        difficulty++;
+      }
+      break;
+    case 'q':
+    case 'Q':
+      exit(0);
+    }
+    printd(difficulty);
+  }
+
+  printf("\n\n");
+
+  MSW_BOARD *board = ctx->board;
+
+  switch (difficulty)
+  {
+    case 1:
+      board->rows = 9;
+      board->cols = 9;
+      break;
+
+    case 2:
+      board->rows = 16;
+      board->cols = 16;
+      break;
+
+    case 3:
+      board->rows = 16;
+      board->cols = 30;
+      break;
+
+    /* dummy */  
+    default:
+      exit(0);
+  }
+
+  board->sx = board->rows/2;
+  board->sy = board->cols/2;
+  ctx->difficulty = difficulty;
+}
+
+void SetBombs (MSW_CTX *ctx)
+{
+  MSW_BOARD *board = ctx->board;
+
+  if (board->rows == 9 && board->cols == 9)
+  {
     board->bombs = 10;
     board->flags = 10;
-  } else if (board->rows == 16 && board->cols == 16) {
+  }
+
+  else if (board->rows == 16 && board->cols == 16)
+  {
     board->bombs = 40;
     board->flags = 40;
-  } else if (board->rows == 16 && board->cols == 30) {
+  }
+
+  else if (board->rows == 16 && board->cols == 30)
+  {
     board->bombs = 99;
     board->flags = 99;
-  } else {
-    int bomb_num = 0;
-    printf("Enter custom number of bombs: ");
-    do {
-      scanf("%d", &bomb_num);
-      if (bomb_num < 1 || bomb_num > (board->rows * board->cols))
-        printf("Invalid number of bombs. Try again.\n");
-    } while (bomb_num > (board->rows * board->cols));
-    board->bombs = bomb_num;
-    board->flags = bomb_num;
   }
 }
 
-int is_valid(BOARD *board, int x, int y) {
-  if (x < 0 || x >= board->rows || y < 0 || y >= board->cols) {
-    return 0;
+MSW_BOOL IsValidPos (MSW_BOARD *board, int x, int y)
+{
+  if (x < 0 || x >= board->rows || y < 0 || y >= board->cols)
+  {
+    return FALSE;
   }
-  return 1;
+  else
+  {
+    return TRUE;
+  }
 }
 
-void place_bombs(BOARD *board) {
-  int bombs = board->bombs;
-  while (bombs > 0) {
-    // int x = rand() % board->rows;
-    int x = ranged(0, board->rows);
-    // int y = rand() % board->cols;
-    int y = ranged(0, board->cols);
-    CELL *cells = *board->cells;
-    if (cells[x * board->cols + y].bomb == 0) {
+void PlaceBombs (MSW_CTX *ctx)
+{
+  MSW_BOARD *board = ctx->board;
+  int i, j, x, y, bombs = board->bombs;
+
+  while (bombs > 0)
+  {
+    x = ranged(0, board->rows - 1);
+    y = ranged(0, board->cols - 1);
+
+    MSW_CELL *cells = *board->cells;
+
+    if (cells[x * board->cols + y].bomb == 0)
+    {
       cells[x * board->cols + y].bomb = 1;
-      for (int i = -1; i <= 1; i++) {
-        for (int j = -1; j <= 1; j++) {
-          if (is_valid(board, x + i, y + j)) {
-            cells[(x + i) * board->rows + (y + j)].value++;
+
+      for (i = -1; i <= 1; ++i)
+      {
+        for (j = -1; j <= 1; ++j)
+        {
+          if (IsValidPos(board, x + i, y + j))
+          {
+            cells[(x + i) * board->cols + (y + j)].value++;
           }
         }
       }
@@ -124,186 +210,375 @@ void place_bombs(BOARD *board) {
   }
 }
 
-void generateBoard(BOARD *board) {
-  num_bombs(board);
-  currboard = malloc(sizeof(CELL) * board->rows * board->cols);
-  board->cells = &currboard;
-  CELL *cells = *board->cells;
+void GenerateBoard (MSW_CTX *ctx)
+{
+  MSW_BOARD *board = ctx->board;
+  if (!board) printf("No board");
+
+  SetBombs(ctx);
+
+  ctx->cur_board = (MSW_CELL*) malloc(sizeof(MSW_CELL) * board->rows * board->cols);
+  board->cells = &(ctx->cur_board);
+  MSW_CELL *cells = *board->cells;
   int i, j;
-  for (i = 0; i < board->rows; i++) {
-    for (j = 0; j < board->cols; j++) {
+
+  for (i = 0; i < board->rows; ++i)
+  {
+    for (j = 0; j < board->cols; ++j)
+    {
       cells[i * board->cols + j].bomb = 0;
       cells[i * board->cols + j].flag = 0;
       cells[i * board->cols + j].clicked = 0;
       cells[i * board->cols + j].value = 0;
     }
   }
-  place_bombs(board);
+
+  PlaceBombs(ctx);
 }
 
-void display(BOARD *board, int winloss) {
-  CELL *cells = *board->cells;
+void Display (MSW_CTX *ctx, int sx, int sy)
+{
+  MSW_BOARD *board = ctx->board;
+  MSW_CELL *cells = *board->cells;
 
-  printf("   ");
-  for (int i = 0; i < board->cols; i++) {
+  MSW_STATE state = CheckWin(ctx);
+
+  system("clear");
+  // clrscr();
+
+  printf("    ");
+  for (int i = 0; i < board->cols; i++)
+  {
     printf("%d ", i);
   }
-  printf("\n");
+  printf("\n\n");
 
-  for (int i = 0; i < board->rows; i++) {
-    printf("%d  ", i);
-    for (int j = 0; j < board->cols; j++) {
-      if (winloss != 0) {
-        if (cells[i * board->cols + j].bomb == 1) {
-          printf("B ");
-        } else if (cells[i * board->cols + j].flag == 1) {
-          printf("F ");
-        } else if (cells[i * board->cols + j].value == 0) {
-          printf("  ");
-        } else {
-          printf("%d ", cells[i * board->cols + j].value);
+  char *bg;
+
+  for (int i = 0; i < board->rows; i++)
+  {
+    printf("%2d  ", i);
+    for (int j = 0; j < board->cols; j++)
+    {
+      if (state != MSW_NONE)
+      {
+        if (cells[i * board->cols + j].bomb == 1)
+        {
+          printf("%s%sB ", BG_RED, FG_BLACK);
         }
-      } else {
-        if (cells[i * board->cols + j].flag == 1) {
-          printf("F ");
-        } else if (cells[i * board->cols + j].clicked == 0) {
-          printf("X ");
-        } else if (cells[i * board->cols + j].bomb == 1) {
-          printf("B ");
-        } else if (cells[i * board->cols + j].value == 0) {
-          printf("  ");
-        } else {
-          printf("%d ", cells[i * board->cols + j].value);
+        else if (cells[i * board->cols + j].flag == 1)
+        {
+          printf("%s%sF ", BG_YELLOW, FG_BLACK);
+        }
+        else if (cells[i * board->cols + j].value == 0)
+        {
+          printf("%s  ", BG_GRAY);
+        }
+        else
+        {
+          printf("%s%s%d ", BG_CYAN_LIGHT, FG_MAGENTA, cells[i * board->cols + j].value);
+        }
+      }
+      
+      else
+      {
+        if (cells[i * board->cols + j].flag == 1)
+        {
+          bg = (i == sx && j == sy) ? BG_BLUE_LIGHT : BG_YELLOW;
+          printf("%s%sF ", bg, FG_BLACK);
+        }
+        else if (cells[i * board->cols + j].clicked == 0)
+        {
+          bg = (i == sx && j == sy) ? BG_BLUE_LIGHT : BG_WHITE;
+          printf("%s%s- ", bg, FG_GRAY);
+        }
+        else if (cells[i * board->cols + j].bomb == 1)
+        {
+          bg = (i == sx && j == sy) ? BG_BLUE_LIGHT : BG_RED;
+          printf("%s%sB ", bg, FG_BLACK);
+        }
+        else if (cells[i * board->cols + j].value == 0)
+        {
+          bg = (i == sx && j == sy) ? BG_BLUE_LIGHT : BG_GRAY;
+          printf("%s  ", bg);
+        }
+        else
+        {
+          bg = (i == sx && j == sy) ? BG_BLUE_LIGHT : BG_CYAN_LIGHT;
+          printf("%s%s%d ", bg, FG_MAGENTA, cells[i * board->cols + j].value);
         }
       }
     }
-    printf("\n");
+    printf("%s\n", FG_BG_CLEAR);
   }
-  printf("Flags remaining: %d\n", board->flags);
+  printf("\n    %s%sFLAGS%s %2d\t", BG_YELLOW, FG_BLACK, FG_BG_CLEAR, board->flags);
+  printf("%s%sTIME%s %4ds\n", BG_GRAY, FG_BLACK, FG_BG_CLEAR, 0);
 }
 
-void click_multi(BOARD *board, int x, int y);
+void Click (MSW_CTX *ctx, int x, int y)
+{
+  MSW_BOARD *board = ctx->board;
+  MSW_CELL *cells = *board->cells;
 
-void click(BOARD *board, int x, int y) {
-  CELL *cells = *board->cells;
-
-  if (cells[x * board->cols + y].bomb == 1) {
-    bombstep = 1;
+  if (cells[x * board->cols + y].bomb == 1)
+  {
+    ctx->bombstep = 1;
     return;
   }
 
   if (cells[x * board->cols + y].flag == 1 ||
-      cells[x * board->cols + y].clicked == 1) {
+      cells[x * board->cols + y].clicked == 1)
+  {
     return;
   }
 
   cells[x * board->cols + y].clicked = 1;
 
-  if (cells[x * board->cols + y].value == 0) {
-    for (int i = -1; i <= 1; i++) {
-      for (int j = -1; j <= 1; j++) {
-        if (is_valid(board, x + i, y + j)) {
-          click(board, x + i, y + j);
+  if (cells[x * board->cols + y].value == 0)
+  {
+    for (int i = -1; i <= 1; i++)
+    {
+      for (int j = -1; j <= 1; j++)
+      {
+        if (IsValidPos(board, x + i, y + j))
+        {
+          Click(ctx, x + i, y + j);
         }
       }
     }
   }
 }
 
-void click_multi(BOARD *board, int x, int y) {
-  CELL *cells = *board->cells;
+void MultiClick (MSW_CTX *ctx, int x, int y)
+{
+  MSW_BOARD* board = ctx->board;
+  MSW_CELL *cells = *board->cells;
   int flagcount = 0;
-  for (int i = -1; i <= 1; i++) {
-    for (int j = -1; j <= 1; j++) {
-      if (is_valid(board, x + i, y + j)) {
+
+  for (int i = -1; i <= 1; i++)
+  {
+    for (int j = -1; j <= 1; j++)
+    {
+      if (IsValidPos(board, x + i, y + j))
+      {
         if (cells[(x + i) * board->cols + (y + j)].flag == 1)
           flagcount++;
       }
     }
   }
-  if (flagcount != cells[x * board->cols + y].value) {
-    printf("Incorect move!\n");
+  if (flagcount != cells[x * board->cols + y].value)
+  {
+    // printf("Incorect move!\n");
     return;
   }
-  for (int i = -1; i <= 1; i++) {
-    for (int j = -1; j <= 1; j++) {
-      if (is_valid(board, x + i, y + j) && cells[(x + i) * board->cols + (y + j)].flag == 0) {
-        click(board, x + i, y + j);
+  for (int i = -1; i <= 1; i++)
+  {
+    for (int j = -1; j <= 1; j++)
+    {
+      if (IsValidPos(board, x + i, y + j) && cells[(x + i) * board->cols + (y + j)].flag == 0)
+      {
+        Click(ctx, x + i, y + j);
       }
     }
   }
 }
 
-void flag(BOARD *board, int x, int y) {
-  CELL *cells = *board->cells;
+void Flag(MSW_CTX *ctx, int x, int y)
+{
+  MSW_BOARD *board = ctx->board;
+  MSW_CELL *cells = *board->cells;
 
-  if (cells[x * board->cols + y].clicked == 1) {
-    printf("Cannot flag a clicked cell.\n");
+  if (cells[x * board->cols + y].clicked == 1)
+  {
+    // printf("Cannot flag a clicked cell.\n");
     return;
   }
 
-  if (cells[x * board->cols + y].flag == 1) {
+  if (cells[x * board->cols + y].flag == 1)
+  {
     cells[x * board->cols + y].flag = 0;
     board->flags++;
-  } else {
+  }
+  else
+  {
     cells[x * board->cols + y].flag = 1;
     board->flags--;
   }
 }
 
-void move(BOARD *board) {
-  printf("Enter move (row, column, f/c) :\n");
-  CELL *cells = *board->cells;
-  int x, y;
-  char f;
-  do {
-    scanf("%d %d %c", &x, &y, &f);
-    if (x < 0 || x >= board->rows || y < 0 || y >= board->cols ||
-        (f != 'c' && f != 'f'))
-      printf("Invalid move. Try again.\n");
-  } while (x < 0 || x >= board->rows || y < 0 || y >= board->cols ||
-           (f != 'c' && f != 'f'));
-  if (f == 'c') {
-    if (cells[x * board->cols + y].flag == 1) {
-      printf("Cannot click a flagged cell.\n");
+void Move (MSW_CTX *ctx)
+{
+  MSW_BOARD *board = ctx->board;
+  MSW_CELL *cells = *board->cells;
+  int sx = board->sx, sy = board->sy;
+  char f = 0, ch = 0;
+
+  Display (ctx, sx, sy);
+
+  /* poll for keypresses */
+  do
+  {
+    ch = _getch();
+    switch (ch)
+    {
+      case 'f':
+      case 'F':
+        f = 'f';
+        break;
+
+      case 'c':
+      case 'C':
+        f = 'c';
+        break;
+      
+      case 'q':
+      case 'Q':
+        exit(0);
+
+      /* up arrow arrow*/
+      case 119:
+        if (IsValidPos(board, sx - 1, sy))
+        {
+          sx--;
+        }
+        break;
+
+      /* right arrow */
+      case 100:
+        if (IsValidPos(board, sx, sy + 1))
+        {
+          sy++;
+        }
+        break;
+
+       /* left arrow */
+      case 97: 
+        if (IsValidPos(board, sx, sy - 1))
+        {
+          sy--;
+        }
+        break;
+
+      /* down arrow */
+      case 115:
+        if (IsValidPos(board, sx + 1, sy))
+        {
+          sx++;
+        }
+        break;
+
+      default:
+        break;
+    }
+    Display(ctx, sx, sy);
+  }
+  while (f != 'c' && f != 'f');
+
+  board->sx = sx;
+  board->sy = sy;
+
+  /* at this point we have a valid cell and operation selected */
+
+  if (f == 'c')
+  {
+    if (cells[sx * board->cols + sy].flag == 1)
+    {
+      // printf("Cannot click a flagged cell.\n");
       return;
     }
-    if (cells[x * board->cols + y].clicked == 1) {
-      click_multi(board, x, y);
-    } else {
-      click(board, x, y);
+    if (cells[sx * board->cols + sy].clicked == 1)
+    {
+      MultiClick(ctx, sx, sy);
     }
-  } else {
-    flag(board, x, y);
+    else
+    {
+      Click(ctx, sx, sy);
+    }
+  }
+  else
+  {
+    Flag(ctx, sx, sy);
   }
 }
 
-int main() {
+MSW_CTX* GetMswCtx (void)
+{
+  MSW_CTX *newCtx = (MSW_CTX*) malloc(sizeof(MSW_CTX));
+  if (!newCtx)
+  {
+    return NULL;
+  }
+
+  newCtx->board = NULL;
+  newCtx->bombstep = 0;
+  newCtx->cur_board = NULL;
+  newCtx->difficulty = -1;
+  
+  return newCtx;
+}
+
+MSW_BOOL NewBoard (MSW_CTX *ctx)
+{
+  if (ctx->board) free(ctx->board);
+  ctx->board = (MSW_BOARD*) malloc(sizeof(MSW_BOARD));
+
+  if (!(ctx->board))
+  {
+    return FALSE;
+  }
+  else
+  {
+    return TRUE;
+  }
+}
+
+
+int main (void) {
+  system("clear");
+  // clrscr();
   randctx();
 
+  MSW_CTX *ctx = GetMswCtx();
+  if (!ctx)
+  {
+    exit(1);
+  }
+
   char c;
-  do {
-    BOARD *board = malloc(sizeof(BOARD));
-    select_difficulty(board);
-    generateBoard(board);
-    do {
-      display(board, checkwin(board));
-      move(board);
-    } while (checkwin(board) == 0);
-    display(board, checkwin(board));
-    if (checkwin(board) == 1) {
-      printf("Congratulations! You win!\n");
-    } else {
+
+  do
+  {
+    if (!NewBoard(ctx)) {printf("exiting"); exit(2);}
+    SelectDifficulty(ctx);
+    GenerateBoard(ctx);
+    // Display(ctx, ctx->board->sx, ctx->board->sy);
+
+    do
+    {
+      Move(ctx);
+    }
+    while (CheckWin(ctx) == MSW_NONE);
+
+    Display(ctx, -1, -1);
+    if (CheckWin(ctx) == MSW_WIN)
+    {
+      printf("You won!\n");
+    }
+    else
+    {
       printf("You lose!\n");
     }
-    printf("Play again? (y/n): \n");
-    getchar();
+
+    printf("Play again? (Y/N): ");
     scanf("%c", &c);
-    if (c == 'n') {
+
+    if (c != 'Y' && c != 'y')
+    {
       break;
     }
-    bombstep=0;
-    free(board);
+
+    ctx->bombstep = 0;
   } while (1);
+
   return 0;
 }
