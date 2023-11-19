1da78af787eafdb66ffe54d72f8e3677c399dca0
Bar0n624 <varun123shenoy@gmail.com>
2023-11-19 14:25:19 +0530

    added seperate leaderboards for difficulties

--- minesweeper-main.c
+++ minesweeper-main.c
@@ -35,17 +35,38 @@ int exists(const char *fname) {
 }
 
 // Global leaderboard variable
-LEADER lboard = {.name = "", .time = 0, .next = NULL, .previous = NULL};
+LEADER lboardeasy = {.name = "", .time = 0, .next = NULL, .previous = NULL};
+LEADER lboardmedium = {.name = "", .time = 0, .next = NULL, .previous = NULL};
+LEADER lboardhard = {.name = "", .time = 0, .next = NULL, .previous = NULL};
 
+LEADER *choose_leader(int difficulty) {
+  switch (difficulty) {
+  case 1:
+    return &lboardeasy;
+  case 2:
+    return &lboardmedium;
+  default:
+    return &lboardhard;
+  }
+}
 // function to initialize the leaderboard from files
-void init_leader() {
+void init_leader(int difficulty) {
   FILE *infile;
-  LEADER *temp = &lboard;
+  LEADER *temp = choose_leader(difficulty);
   int i = 0;
   while (1) {
     char filename[32];
-    snprintf(filename, 32, "leader%d.csv", i);
-
+    switch (difficulty) {
+    case 1:
+      snprintf(filename, 32, "easy%d.csv", i);
+      break;
+    case 2:
+      snprintf(filename, 32, "medium%d.csv", i);
+      break;
+    default:
+      snprintf(filename, 32, "hard%d.csv", i);
+      break;
+    }
     if (exists(filename)) {
       infile = fopen(filename, "r");
       if (i == 0) {
@@ -71,8 +92,8 @@ void init_leader() {
 }
 
 // function to insert a new entry into the leaderboard
-void insert_leader(char name[], int time) {
-  LEADER *temp = &lboard;
+void insert_leader(char name[], int time, int difficulty) {
+  LEADER *temp = choose_leader(difficulty);
 
   if (temp->next == NULL) {
     // If the list is empty, insert as the first node
@@ -103,9 +124,10 @@ void insert_leader(char name[], int time) {
 }
 
 // function to display the leaderboard
-void display_leader() {
+void display_leader(int difficulty) {
   printf("\n\n Leaderboard\n");
-  LEADER *temp = lboard.next;
+  LEADER *lboard = choose_leader(difficulty);
+  LEADER *temp = lboard->next;
   int i = 1;
   while (temp != NULL) {
     printf("%d. %s\t%d\n", i, temp->name, temp->time);
@@ -115,13 +137,24 @@ void display_leader() {
 }
 
 // writes all the leaderboard contents to csv files
-void write_leader() {
+void write_leader(int difficulty) {
   FILE *outfile;
-  LEADER *temp = lboard.next;
+  LEADER *lboard = choose_leader(difficulty);
+  LEADER *temp = lboard->next;
   int i = 0;
   while (temp != NULL) {
     char filename[32];
-    snprintf(filename, 32, "leader%d.csv", i);
+    switch (difficulty) {
+    case 1:
+      snprintf(filename, 32, "easy%d.csv", i);
+      break;
+    case 2:
+      snprintf(filename, 32, "medium%d.csv", i);
+      break;
+    default:
+      snprintf(filename, 32, "hard%d.csv", i);
+      break;
+    }
     outfile = fopen(filename, "w");
     fprintf(outfile, "%s,%d\n", temp->name, temp->time);
     fclose(outfile);
@@ -283,8 +316,8 @@ void PlaceBombs(MSW_CTX *ctx) {
         0) { // Checks if a bomb already exists
       cells[x * board->cols + y].bomb = 1;
 
-      for (i = -1; i <= 1; ++i) { // Iteratively increases the bomb value of all
-                                  // cells around the bomb
+      for (i = -1; i <= 1; ++i) { // Iteratively increases the bomb value of
+                                  // all cells around the bomb
         for (j = -1; j <= 1; ++j) {
           if (IsValidPos(board, x + i, y + j)) {
             cells[(x + i) * board->cols + (y + j)].value++;
@@ -323,12 +356,11 @@ void GenerateBoard(MSW_CTX *ctx) {
 
 // Function to display the board
 void Display(MSW_CTX *ctx, int sx, int sy) {
+  system("clear");
   MSW_BOARD *board = ctx->board;
   MSW_CELL *cells = *board->cells;
 
   MSW_STATE state = CheckWin(ctx);
-
-  system("clear");
 
   printf("\n\n\n███    ███ ██ ███    ██ ███████ ███████ ██     ██ ███████ "
          "███████ ██████  ███████ ██████\n"
@@ -381,7 +413,7 @@ void Display(MSW_CTX *ctx, int sx, int sy) {
     }
     printf("%s\n", FG_BG_CLEAR);
   }
-  printf("\n    %s%sFLAGS%s %2d\t", BG_YELLOW, FG_BLACK, FG_BG_CLEAR,
+  printf("\n %s%sFLAGS%s %2d\t \n\n\n", BG_YELLOW, FG_BLACK, FG_BG_CLEAR,
          board->flags);
 }
 
@@ -414,7 +446,8 @@ void Click(MSW_CTX *ctx, int x, int y) {
   }
 }
 
-// Function to click all the cells around a cell, if all expected flags are met
+// Function to click all the cells around a cell, if all expected flags are
+// met
 void MultiClick(MSW_CTX *ctx, int x, int y) {
   MSW_BOARD *board = ctx->board;
   MSW_CELL *cells = *board->cells;
@@ -570,8 +603,7 @@ MSW_BOOL NewBoard(MSW_CTX *ctx) {
 
 // main function
 int main(void) {
-  int timer = 0; // sets the timer to 0
-  init_leader(); // loads the leaderboard
+  int timer = 0; // sets the timer to 0 // loads the leaderboard
   system("clear");
   randctx();
 
@@ -587,6 +619,7 @@ int main(void) {
     }
     SelectDifficulty(ctx);
     GenerateBoard(ctx);
+    init_leader(ctx->difficulty);
     time_t start = time(NULL); // gets start time
     do {                       // game loop
       Move(ctx);
@@ -606,9 +639,9 @@ int main(void) {
       char name[20];
       printf("Enter your name: ");
       scanf("%s", name);
-      insert_leader(name, timer); // inserts into leaderboard
-      write_leader();
-      display_leader();
+      insert_leader(name, timer, ctx->difficulty); // inserts into leaderboard
+      write_leader(ctx->difficulty);
+      display_leader(ctx->difficulty);
     } else {
       printf("\n\n"
              "▓██   ██▓ ▒█████   █    ██     ██▓     ▒█████    ██████ ▓█████\n"
@@ -623,13 +656,14 @@ int main(void) {
              " ░ ░                                                           "
              "\n\n");
       printf("Time taken is : %.0f seconds\n", (double)(time(NULL) - start));
-      display_leader();
+      display_leader(ctx->difficulty);
     }
 
     fflush(stdin);
     printf("Play again? (Y/N): ");
     fflush(stdin);
-    scanf("%c", &c);
+    getchar();
+    c = getchar();
     if (c == 'N' || c == 'n') {
       break;
     }
